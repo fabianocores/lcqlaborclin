ca7d92a0865bd9836ce50c7571527ae820734376
fabianocores <fabiano@arkabit.com>
2017-12-20 20:54:44 -0200

    Carga

---
+++ www/config/appConfig.js
@@ -0,0 +1,23 @@
+﻿// constante para instalação WEB, consumido pelo serviço urlConfig (appConfigKeys.js - coloquei neste script para uglify e unificação)
+angular.module("arkabPortal").constant('urlConfigParams', {
+    config: {
+
+        // endereço da API do cliente, sempre com /api/ no final.
+        //baseUrl: "https://localhost:44338/api/",
+        baseUrl: 'https://192.168.0.13:8080/WebAPITempPublish/api/',
+
+        // nome do domínio da web api.
+        dataProfile: "SBO_Laborclin",
+
+        // propriedade no google analytics.
+        googleAnalyticsChild: '1', 
+
+        partnerConfig: {
+            name: 'CVA Consultoria',
+            site: 'http://www.cvaconsultoria.com.br',
+            fone: '3030-4040',
+            logo: 'cva', // utilize arquivos "partnerLogo"_20.png e _60.png
+        },
+    }
+});
+
